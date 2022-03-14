a50723cc600c2a4ec1c8f2fcf388936eab2461e4
Thato1016 <tb.khosa@gmail.com>
2022-03-14 11:53:51 +0200

    printf

--- main.h
+++ main.h
@@ -1,9 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _LIBRARIES_
+#define _LIBRARIES_
 #include <stdio.h>
-#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#endif
 
+#ifndef _MAIN_
+#define _MAIN_H_
+
+/**
+ *struct functions - Struct of formats and functions
+ *@lf: For letter format
+ *@f: function print someting.
+ */
+typedef struct functions
+{
+	char *lf;
+	int (*f)(va_list selector);
+} prin;
 int _printf(const char *format, ...);
-
+int print_numd(va_list selector);
+int _char(va_list selector);
+int _str(va_list selector);
+int print_number(va_list selector);
+int print_unsigned(va_list selector);
 #endif
