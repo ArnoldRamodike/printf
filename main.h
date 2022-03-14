445485135f4ec98fb5e1a2d4926702a596f34a03
Thato1016 <tb.khosa@gmail.com>
2022-03-14 15:56:06 +0200

    printf

--- main.h
+++ main.h
@@ -1,18 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>                                                                                                                                                                     
+#include <stdio.h>
+#include <stdarg.h>                                                                                                                                                                
                                                                                                                                                                                               
 int printf(const char *format, ...);                                                                                                                                                   
 int fprintf(FILE *stream, const char *format, ...);                                                                                                                                    
 int dprintf(int fd, const char *format, ...);                                                                                                                                          
 int sprintf(char *str, const char *format, ...);                                                                                                                                       
-int snprintf(char *str, size_t size, const char *format, ...);                                                                                                                         
-                                                                                                                                                                                              
-#include <stdarg.h>                                                                                                                                                                    
-                                                                                                                                                                                              
+int snprintf(char *str, size_t size, const char *format, ...);
 int vprintf(const char *format, va_list ap);                                                                                                                                           
 int vfprintf(FILE *stream, const char *format, va_list ap);                                                                                                                            
 int vdprintf(int fd, const char *format, va_list ap);                                                                                                                                  
 int vsprintf(char *str, const char *format, va_list ap);                                                                                                                               
 int vsnprintf(char *str, size_t size, const char *format, va_list ap); 
+
 #endif
