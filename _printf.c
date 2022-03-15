ee3c44c05a5f5c07f8b3967f7536373bed60e20e
Thato1016 <tb.khosa@gmail.com>
2022-03-15 13:17:54 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -1,120 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _printf - function my printf
+ * @format: string whit format to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_numbers - prints a number
- * @n: integer to be printed
- * Return: integer
- */
-
-int print_numbers(int n)
-{
-	unsigned int number;
-	int i = 0;
-
-	if (n < 0)
-	{
-		number = -n;
-		i = i + _putchar('-');
-	}
-	else
-	{
-		number = n;
-	}
-	if (number / 10 != '\0')
-		i = i + print_numbers(number / 10);
-	i = i + _putchar(number % 10 + '0');
-	return (i);
-}
-
-/**
- * print_unsigned - prints an unsigned integer
- * @number: unsigned integer to be printed
- * @mode: identifies base notation: 8, 10 or 16
- * Return: integer
- */
-
-int print_unsigned(unsigned int number, int mode)
-{
-	int i = 0;
-
-	if (number / mode != '\0')
-		i = i + print_numbers(number / mode);
-	i = i + _putchar(number % mode + '0');
-	return (i);
-}
-
-/**
- * _printf_string - the function printfstring
- *@tstring:char the pointer
- * Return: Always 0
- */
-int _printf_string(char *tstring)
-{
-	int j = 0;
-
-	do {
-		_putchar(tstring[j]);
-		j++;
-	} while (tstring[j] != '\0');
-	return (j);
-}
-/**
- * _printf - the function printf
- *@format:char the pointer
- * Return: Always 0
+ * Return: number of chars that print
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, k = 0;
-	char type;
+	va_list args;
+	int length = 0;
 
-	va_list list;
+	if (format == NULL)
+		return (-1);
 
-	va_start(list, format);
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			type = format[++i];
-			if (type == 'c')
-				k = k + _putchar((char)va_arg(list, int));
-			if (type == 's')
-				k = k + _printf_string(va_arg(list, char*));
-			if (type == '%')
-				k = k + _putchar('%');
-			if (type == 'd' || type == 'i')
-				k = k + print_numbers(va_arg(list, int));
-			if (type == 'u')
-				k = k + print_unsigned
-					(va_arg(list, unsigned int), 10);
-			if (type == 'o')
-				k = k + print_unsigned
-					(va_arg(list, unsigned int), 8);
-			if (type == 'x' || type == 'X')
-				k = k + print_unsigned
-					(va_arg(list, unsigned int), 16);
-			if (type != 'c' && type != 's' && type != '%'
-			    && type != 'd' && type != 'i' && type != 'u'
-			    && type != 'o' && type != 'x' && type != 'X')
-				k = k + _putchar('%') + _putchar(type);
-		}
-		else
-			k = k + _putchar(format[i]);
-		i++;
-	}
-	return (k);
+	va_start(args, format);
+
+	length = _print_format(format, args);
+	va_end(args);
+	return (length);
 }
--- main.h
+++ main.h
@@ -1,17 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdarg.h>                                                                                                                                                                
-                                                                                                                                                                                              
-int printf(const char *format, ...);                                                                                                                                                   
-int fprintf(FILE *stream, const char *format, ...);                                                                                                                                    
-int dprintf(int fd, const char *format, ...);                                                                                                                                          
-int sprintf(char *str, const char *format, ...);                                                                                                                                       
-int snprintf(char *str, size_t size, const char *format, ...);
-int vprintf(const char *format, va_list ap);                                                                                                                                           
-int vfprintf(FILE *stream, const char *format, va_list ap);                                                                                                                            
-int vdprintf(int fd, const char *format, va_list ap);                                                                                                                                  
-int vsprintf(char *str, const char *format, va_list ap);                                                                                                                               
-int vsnprintf(char *str, size_t size, const char *format, va_list ap); 
+#include <stdarg.h>
 
-#endif
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	int (*f)(va_list args);
+} spc_dt;
+
+int _write(char c);
+int _printf(const char *format, ...);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
+
+#endif /* _MAIN */
