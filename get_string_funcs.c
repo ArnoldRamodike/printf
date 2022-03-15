ddc7f2f08a5f8c9347e0b40890e44153d81a51a6
ArnoldRamodike <95076110+ArnoldRamodike@users.noreply.github.com>
2022-03-15 15:23:46 +0200

    Update get_string_funcs.c

--- get_string_funcs.c
+++ get_string_funcs.c
@@ -6,28 +6,4 @@
  *
  * Return: pointer to a function that takes va_list and returns char *
  */
-char *(*get_string_func(char ch))(va_list, char *mods)
-{
-	spec_t specs[] = {
-		{ 'c', get_char },
-		{ 's', get_string },
-		{ 'S', get_nonprint_string },
-		{ '%', get_percent },
-		{ 'd', get_int },
-		{ 'i', get_int },
-		{ 'b', get_binary },
-		{ 'u', get_unsigned },
-		{ 'o', get_octal },
-		{ 'x', get_hex },
-		{ 'X', get_hex_upper },
-		{ 'p', get_pointer },
-		{ 'r', get_reverse },
-		{ 'R', get_rot },
-		{ NULL, NULL }
-	};
-	int i = 0;
 
-	while (specs[i].t != ch)
-		i++;
-	return (specs[i].f);
-}
