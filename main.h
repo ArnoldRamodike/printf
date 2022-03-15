fe9f5e365ddfe0a7716aed7f1fc565222c965eba
Thato1016 <tb.khosa@gmail.com>
2022-03-15 13:41:14 +0200

    printf

--- main.h
+++ main.h
@@ -1,29 +1,37 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <limits.h>
+#include <unistd.h>
 #include <stdarg.h>
-
+#include <stdlib.h>
+#include <unistd.h>
 /**
-  * struct specifiers - Struct specifiers
-  * @specifier: The conversion specifier
-  * @f: The function pointer
-  */
-typedef struct specifiers
+ * struct printf - struct printf
+ * @c: The character
+ * @fun: The function associated
+ */
+typedef struct printf
 {
-	char *specifier;
-	int (*f)(va_list args);
-} spc_dt;
+	char c;
+	int (*fun)(va_list);
+} printf_t;
 
-int _write(char c);
 int _printf(const char *format, ...);
-int _print_a_char(va_list args);
-int _print_a_string(va_list args);
-int _print_format(const char *format, va_list args);
-int _print_spec(char format, va_list args);
-int _print_invalid_spec(char prev_format, char format, int count);
-int _print_a_integer(va_list args);
-void _recursion_integer(int a);
-int _print_int_binary(va_list args);
-void _recursion_int_binary(int a);
-int _validate_char(char _type);
+int _putchar(char c);
+int get_func(const char s, va_list ap);
 
-#endif /* _MAIN */
+
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int print_int(va_list arg);
+int print_binaire(va_list b);
+int print_unsf(va_list arg);
+int print_oct(va_list arg);
+int print_hex(va_list arg);
+int print_heX(va_list arg);
+int _rot13(va_list ap);
+int _printrev(va_list ap);
+int print_P(va_list p);
+
+#endif/* PRINTF_H */
